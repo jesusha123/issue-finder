50178dd02a51e0a2821605b616bbf19aea0c0c14
Jesus Herrera <chuy.max@gmail.com>
2020-06-21 12:51:32 -0400

    Refactor find logic to own file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
 import IssueTable from './IssueTable'
-import { Octokit } from '@octokit/rest'
-
-
-const octokit = new Octokit();
+import findIssues from './IssueFinder'
 
 class App extends React.Component {
   constructor(props) {
@@ -32,37 +29,16 @@ class App extends React.Component {
     }, this.refreshIssues);
   }
 
+  handleHelpWantedChange(event) {
+    console.log("Changing help wanted")
+  }
+
   refreshIssues() {
-    let base_parms = [
-      "repo:kubernetes/kubernetes",
-      "is:issue",
-      "is:open",
-      "no:assignee"
-    ]
-
-    const q = base_parms.join(" ") + " " + this.state.authors.map(a => "author:"+a).join(" ")
-
-    octokit.search
-      .issuesAndPullRequests({ q })
-      .then(({ data }) => {
-        console.log(data)
-        let issues = data.items.map((item) => ({ 
-          number: item.number,
-          title: item.title,
-          author: item.user.login,
-          labels: item.labels.map(l => ({ 
-            id: l.id, 
-            name: l.name,
-            color: l.color
-          })),
-          date: item.created_at,
-          link: "https://github.com/kubernetes/kubernetes/issues/"+item.number,
-        }))
-
-        this.setState({
-          issues: issues
-        });
+    findIssues(this.state.authors, (issues) => {
+      this.setState({
+        issues: issues
       });
+    })
   }
 
   componentDidMount() {
@@ -74,7 +50,7 @@ class App extends React.Component {
       <div className="App">
         <label htmlFor="authorsInput">Authors</label>
         <input type="text" id="authorsInput" placeholder="Authors" value={this.state.authors} onChange={this.handleAuthorInput} />
-        <input type="checkbox" value="" id="helpWantedCheckbox" />
+        <input type="checkbox" value="" id="helpWantedCheckbox" onChange={this.handleHelpWantedChange} />
         <label htmlFor="helpWantedCheckbox">Help Wanted</label>
         <IssueTable issues={this.state.issues} />
       </div>
---
+++ src/IssueFinder.js
@@ -0,0 +1,36 @@
+import { Octokit } from '@octokit/rest'
+
+const octokit = new Octokit();
+
+function findIssues(authors, callback) {
+  let base_parms = [
+    "repo:kubernetes/kubernetes",
+    "is:issue",
+    "is:open",
+    "no:assignee"
+  ]
+
+  const q = base_parms.join(" ") + " " + authors.map(a => "author:"+a).join(" ")
+
+  octokit.search
+    .issuesAndPullRequests({ q })
+    .then(({ data }) => {
+      console.log(data)
+      let issues = data.items.map((item) => ({ 
+        number: item.number,
+        title: item.title,
+        author: item.user.login,
+        labels: item.labels.map(l => ({ 
+          id: l.id, 
+          name: l.name,
+          color: l.color
+        })),
+        date: item.created_at,
+        link: "https://github.com/kubernetes/kubernetes/issues/"+item.number,
+      }))
+
+      callback(issues)
+    });
+}
+
+export default findIssues
