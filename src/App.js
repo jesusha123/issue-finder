6425b3451ae013096d3792e4fe1ccda5e548068f
Jesus Herrera <chuy.max@gmail.com>
2020-06-21 11:28:31 -0400

    Lift GH search up to app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
 import React from 'react';
 import './App.css';
 import IssueTable from './IssueTable'
+import { Octokit } from '@octokit/rest'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       issues: [],
+      authors: [
+        "BenTheElder",
+        "liggitt",
+        "dims",
+        "Huang-Wei",
+        "saad-ali",
+        "verult"
+      ]
     };
   }
 
-  render() {
-    let authors = [
-      "BenTheElder",
-      "liggitt",
-      "dims",
-      "Huang-Wei",
-      "saad-ali",
-      "verult"
+  componentDidMount() {
+    const octokit = new Octokit();
+
+    let base_parms = [
+      "repo:kubernetes/kubernetes",
+      "is:issue",
+      "is:open",
+      "no:assignee"
     ]
 
+    const q = base_parms.join(" ") + " " + this.state.authors.map(a => "author:"+a).join(" ")
+
+    octokit.search
+      .issuesAndPullRequests({ q })
+      .then(({ data }) => {
+        console.log(data)
+        let issues = data.items.map((item) => ({ 
+          number: item.number,
+          title: item.title,
+          author: item.user.login,
+          labels: item.labels.map(l => ({ 
+            id: l.id, 
+            name: l.name,
+            color: l.color
+          })),
+          date: item.created_at,
+          link: "https://github.com/kubernetes/kubernetes/issues/"+item.number,
+        }))
+
+        this.setState({
+          issues: issues
+        });
+      });
+  }
+
+  render() {
     return (
       <div className="App">
-        <input class="form-control" type="text" placeholder="Authors"></input>
-        <IssueTable authors={authors} />
+        <input class="form-control" type="text" placeholder="Authors" value={this.state.authors} />
+        <IssueTable issues={this.state.issues} />
       </div>
     );
   }
--- src/IssueTable.js
+++ src/IssueTable.js
@@ -1,53 +1,14 @@
 import React from 'react';
 import './IssueTable.css';
-import { Octokit } from '@octokit/rest'
 import IssueRow from './IssueRow';
 
 class IssueTable extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      issues: [],
-    };
-  }
-
-  componentDidMount() {
-    const octokit = new Octokit();
-
-    let base_parms = [
-      "repo:kubernetes/kubernetes",
-      "is:issue",
-      "is:open",
-      "no:assignee"
-    ]
-
-    const q = base_parms.join(" ") + " " + this.props.authors.map(a => "author:"+a).join(" ")
-
-    octokit.search
-      .issuesAndPullRequests({ q })
-      .then(({ data }) => {
-        console.log(data)
-        let issues = data.items.map((item) => ({ 
-          number: item.number,
-          title: item.title,
-          author: item.user.login,
-          labels: item.labels.map(l => ({ 
-            id: l.id, 
-            name: l.name,
-            color: l.color
-          })),
-          date: item.created_at,
-          link: "https://github.com/kubernetes/kubernetes/issues/"+item.number,
-        }))
-
-        this.setState({
-          issues: issues
-        });
-      });
   }
 
   render() {
-    const listItems = this.state.issues.map((issue) => 
+    const listItems = this.props.issues.map((issue) => 
       <IssueRow key={issue.number} issue={issue} />
     );
 
